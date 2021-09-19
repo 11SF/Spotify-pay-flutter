ae12d8d5e828f482982b97544f1053c5c54d6660
11SF <nsfolknsf@gmail.com>
2021-09-20 05:21:01 +0700

    delete flutter svg!!!!!!!!!

--- lib/pages/pay_page.dart
+++ lib/pages/pay_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+// import 'package:flutter_svg/svg.dart';
 
 class Pay extends StatefulWidget {
   Pay({Key? key}) : super(key: key);
